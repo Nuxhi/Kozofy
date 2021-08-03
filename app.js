ce685b036ac64003bee2c38ef888e56db3b9b88c
Reiiko2b <88282329+Reiiko2b@users.noreply.github.com>
2021-08-03 12:38:24 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-const text = ["Kozofy, The Freedom Platform\n starting now."];
+const text = ["Kozofy, The Freedom Platform \n starting now."];
 let letter = "";
 let index = 0;
 let count = 0;
 let currentText = "";
 
 (function type() {
-    if(count === text.length) {
-        count = 0;
-    }
     currentText = text[count];
     letter = currentText.slice(0,++index);
 
@@ -17,5 +14,5 @@ if(letter.length === currentText.length) {
     count++;
     index = 0;
     }
-    setTimeout(type, 300);
+    setTimeout(type, 100);
 })();
